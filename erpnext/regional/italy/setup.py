4a91b7a1bf6b03a5d5b198c3b74f151c1b97cbcd
借书人 <45583218+jieshuren@users.noreply.github.com>
2026-04-23 23:50:21 +0800

    Update Chinese translation and add Italian regional module

---
+++ erpnext/regional/italy/setup.py
@@ -0,0 +1,10 @@
+def make_custom_fields():
+	pass
+
+
+def setup_report():
+	pass
+
+
+def add_permissions():
+	pass
---
+++ erpnext/regional/italy/utils.py
@@ -0,0 +1,18 @@
+def sales_invoice_on_submit(doc, method=None):
+	pass
+
+
+def sales_invoice_on_cancel(doc, method=None):
+	pass
+
+
+def set_state_code(doc, method=None):
+	pass
+
+
+def update_itemised_tax_data(doc):
+	pass
+
+
+def sales_invoice_validate(doc, method=None):
+	pass
